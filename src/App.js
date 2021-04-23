d0caeb1368916be968f03c5dc97832cd589ce400
kavishshah-Bacancy <kavish.shah@bacancy.com>
2021-04-23 16:14:01 +0530

    Logic Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [like, setLike] = useState(100);
+  const [disLike, setDisLike] = useState(25);
+  const [likeflag, setLikeFlag] = useState(false);
+  const [disLikeflag, setDisLikeFlag] = useState(false);
+
+  const likeClass = [];
+  likeClass.push("like-button");
+  const updateLikeHandler = () => {
+    if (likeflag) {
+      setLike(like - 1);
+      setLikeFlag(!likeflag);
+    } else {
+      setLike(like + 1);
+      setLike(!likeflag);
+      if (disLikeflag) {
+        setDisLike(disLike - 1);
+        setDisLikeFlag(!disLikeflag);
+      }
+    }
+  };
+  const updateDisLikeHandler = () => {
+    if (disLikeflag) {
+      setDisLike(like - 1);
+      setDisLikeFlag(!disLikeflag);
+    } else {
+      setDisLike(disLike + 1);
+      setDisLike(!disLikeflag);
+      if (likeflag) {
+        setLike(like - 1);
+        setLikeFlag(!likeflag);
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        className={likeflag ? "like-button liked" : "like-button"}
+        onClick={updateLikeHandler}
+      >
+        Like | {like}
+      </button>
+      <button
+        className={disLikeflag ? "dislike-button disliked" : "dislike-button"}
+        onClick={updateDisLikeHandler}
+      >
+        Dislike | {disLike}
+      </button>
     </div>
   );
 }
